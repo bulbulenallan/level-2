3a5181998beee4902616a6188b17797793dd86a2
allan kipchumba <108665468+bulbulenallan@users.noreply.github.com>
2023-02-13 06:36:25 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+//if statement syntax
+let username = false;
+if(username == true){
+  username +=1;
+  console.log("Identfied as ",username,"of ours");
+}
+ else {
+   username = 0 ;
+   console.log("We dont recognize",username,"of us");
+   
+ }
+ 
+
